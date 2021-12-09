52577f7f4f3ada2e60f59687a45b0c91090af1e6
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-12-08 20:35:01 -0400

    Criação de método de saque

--- src/index.js
+++ src/index.js
@@ -12,23 +12,93 @@ const app = express();
 
 app.use(express.json());
 
-const custumers = [];
+const customers = [];
+
+// Middleware - Verificar se o CPF informado no header existe na lista
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found."});
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const customerAlreayExists = custumers.some((customer) => customer.cpf === cpf);
+  const customerAlreayExists = customers.some((customer) => customer.cpf === cpf);
 
   if(customerAlreayExists) {
-    return response.status(400).json({ error: "Customer already exists!" });
+    return response.status(400).json({ error: "Customer already exists." });
   }
 
-  custumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidv4(),
     statement: []
   });
+
+  return response.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount, description } = request.body;
+  
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds.'});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation);
 
   return response.status(201).send();
 });
